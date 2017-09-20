d2757e6915ced92922172aa0fbc4d67da1f051e8
Stuart <stuin01@gmail.com>
2017-09-20 12:17:41 -0700

    Moved audio functions to static place and enforced normalized formatting

--- Scripts/AudioSingleton.cs
+++ Scripts/AudioSingleton.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class AudioSingleton : MonoBehaviour {
-    
+
     private static AudioClip openingTrack, loopTrack, creditTrack;
-	private AudioSource audioSource;
+    private AudioSource audioSource;
     private static AudioSingleton instance = null;
-	 
+
+    //Audio BS
+    private Image volumeImage;
+    private Sprite imgMuted, imgUnmuted;
+    private AudioSource music;
+
     public static AudioSingleton Instance {
         get { return instance; }
     }
-	 
+
     void Awake() {
         if (instance != null && instance != this) {
             Destroy(this.gameObject);
@@ -18,29 +23,61 @@ public class AudioSingleton : MonoBehaviour {
         } else {
             instance = this;
         }
-	 
+
         DontDestroyOnLoad(this.gameObject);
 
-		return;
-     }
-     
-     void Start(){
-		GameObject go = GameObject.Find ("Music");
-		audioSource = go.GetComponent<AudioSource>();
+        return;
+    }
 
-		//openingTrack = Resources.Load<AudioClip> ("Music/opening theme music");
-		loopTrack = Resources.Load<AudioClip> ("Music/card interaction 1");
-		creditTrack = Resources.Load<AudioClip>("Music/credits guidance");
+    void Start() {
+        GameObject go = GameObject.Find("Music");
+        audioSource = go.GetComponent<AudioSource>();
 
-		return;
-     }
-     
-	void Update() {
-		if (!audioSource.isPlaying) {
-			audioSource.clip = loopTrack;
-			audioSource.Play();
-		}
+        //openingTrack = Resources.Load<AudioClip> ("Music/opening theme music");
+        loopTrack = Resources.Load<AudioClip>("Music/card interaction 1");
+        creditTrack = Resources.Load<AudioClip>("Music/credits guidance");
 
-		return;
-	}
-} 
+        AudioSetup();
+
+        return;
+    }
+
+    void Update() {
+        if (!audioSource.isPlaying) {
+            audioSource.clip = loopTrack;
+            audioSource.Play();
+        }
+
+        return;
+    }
+
+    public void AudioSetup() {
+        GameObject volumeBtn = GameObject.Find("VolumeBtn");
+        volumeImage = volumeBtn.GetComponent<Image>();
+
+        imgMuted = Resources.Load<Sprite>("Buttons/SoundOn");
+        imgUnmuted = Resources.Load<Sprite>("Buttons/SoundOff");
+
+        GameObject volumeObj = GameObject.Find("Music");
+        music = volumeObj.GetComponent<AudioSource>();
+
+        if (music.volume.Equals(0f)) {
+            //Debug.Log("Ch-ch-ch-ch-changes");
+            volumeImage.sprite = imgUnmuted;
+        }
+
+        return;
+    }
+
+    public void VolumeControl() {
+        if (music.volume.Equals(0f)) {
+            volumeImage.sprite = imgMuted;
+            music.volume = 1f;
+        } else {
+            volumeImage.sprite = imgUnmuted;
+            music.volume = 0f;
+        }
+
+        return;
+    }
+}
--- Scripts/UIManagerScript.cs
+++ Scripts/UIManagerScript.cs
@@ -4,86 +4,53 @@ using UnityEngine.UI;
 
 public class UIManagerScript : MonoBehaviour {
 
-	private Image volumeImage;
-	private Sprite imgMuted, imgUnmuted;
-	private AudioSource music;
-	
-	public void Start(){
-		AudioSetup ();
+    public void Start() {
+        AudioSingleton.instance.AudioSetup();
 
-		return;
-	}
+        return;
+    }
 
-	public void Update(){
-		//Checking platform and scene *** Change when compiling to iOS ***
-		if ((Application.platform == RuntimePlatform.Android) && (Application.loadedLevelName != "Menu")) {
-				if (Input.GetKeyDown (KeyCode.Escape)) {
-					Application.LoadLevel ("Menu");
+    public void Update() {
+        //Checking platform and scene *** Change when compiling to iOS ***
+        if ((Application.platform == RuntimePlatform.Android) && (Application.loadedLevelName != "Menu")) {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                Application.LoadLevel("Menu");
 
-					return;
-				}
-		} else if (Application.loadedLevelName == "Menu") {
-				if (Input.GetKeyDown (KeyCode.Escape)) {
-					Application.Quit ();
+                return;
+            }
+        } else if (Application.loadedLevelName == "Menu") {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                Application.Quit();
 
-					return;
-				}
-		}
-	}
+                return;
+            }
+        }
+    }
 
-	public void getCards()
-	{
-		Application.LoadLevel("DrawCard");
-		
-		return;
-	}
-	public void getSpread()
-	{
-		Application.LoadLevel("Spread");
-		
-		return;
-	}
-	public void getDeck()
-	{
-		Application.LoadLevel("Explore");
-		
-		return;
-	}
-	public void getGuidance()
-	{
-		Application.LoadLevel("Guidance");
-		
-		return;
-	}
+    public void getCards() {
+        Application.LoadLevel("DrawCard");
+        return;
+    }
+    public void getSpread() {
+        Application.LoadLevel("Spread");
 
-	private void AudioSetup(){		
-		GameObject volumeBtn = GameObject.Find("VolumeBtn");
-		volumeImage = volumeBtn.GetComponent<Image>();
-		
-		imgMuted = Resources.Load<Sprite>("Buttons/SoundOn");
-		imgUnmuted = Resources.Load<Sprite>("Buttons/SoundOff");
-		
-		GameObject volumeObj = GameObject.Find("Music");
-		music = volumeObj.GetComponent<AudioSource>();
+        return;
+    }
+    public void getDeck() {
+        Application.LoadLevel("Explore");
 
-		if (music.volume.Equals(0f)) {
-			//Debug.Log("Ch-ch-ch-ch-changes");
-			volumeImage.sprite = imgUnmuted;		
-		}
-		
-		return;
-	}
-	
-	public void VolumeControl(){
-		if(music.volume.Equals(0f)){
-			volumeImage.sprite = imgMuted;
-			music.volume = 1f;
-		} else {
-			volumeImage.sprite = imgUnmuted;
-			music.volume = 0f;
-		}
-		
-		return;
-	}
+        return;
+    }
+    public void getGuidance() {
+        Application.LoadLevel("Guidance");
+
+        return;
+    }
+
+    public void VolumeControl() {
+        AudioSingleton.instance.VolumeControl();
+
+        return;
+    }
 
 }
